8a0e4984ea1cd761f76a8edd6a0f175aa903f93a
Vinny Nguyen <61941028+SirFinleyNewman@users.noreply.github.com>
2025-02-20 19:32:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const alternatives = [
   {text:"Why are you back here Gabi!!! Ad Astra Per Aspera!", images:"images/Excited Happy.gif"},
-  {text:"It's good that youre nervous! Moving across the nation is no joke.", images:"images/Believe in You.gif"},
   {text:"There is no what if, I know you'll be able to", images:"images/Encouragement.gif"},
   {text:"You won't have to do this alone, you know", images:"images/Im rooting for you.gif"},
   {text:"Everyone will always be here supporting you, no matter what!", images:"images/Believe in You.gif"},
-  {text:"We wont forget you, how lucky are all of us?", images:"images/Goodbye"},
+  {text:"We wont forget you, how lucky are all of us?", images:"images/Goodbye.jpeg"},
   {text:"Don't turn back now Gabi, I know you can do this", images:"images/Im so proud.gif"},
 ]
 const ohyes = {text:"Don't worry about me Gabi. I'll be okay, you're my friend till the end. I'll be here rooting for you wherever and whenever", images:"images/Friends Forever.jpg"}
